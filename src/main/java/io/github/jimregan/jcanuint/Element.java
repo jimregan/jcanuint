e6183a0f6dd2eeb52aa28da6a3321bae41c48e6c
Jim O'Regan <jaoregan@tcd.ie>
2018-08-09 18:23:14 +0100

    start

---
+++ src/main/java/io/github/jimregan/jcanuint/Element.java
@@ -0,0 +1,65 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018 Jim O'Regan
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package io.github.jimregan.jcanuint;
+
+public class Element {
+    private double C;
+    private double M;
+    private double U;
+
+    public double getM() {
+        return M;
+    }
+
+    public void setM(double m) {
+        M = m;
+    }
+
+    public double getC() {
+        return C;
+    }
+
+    public void setC(double c) {
+        C = c;
+    }
+
+    public double getU() {
+        return U;
+    }
+
+    public void setU(double u) {
+        U = u;
+    }
+
+    public boolean isZero() {
+        return (M == C && C == U && U == 0.0);
+    }
+
+    public Element(double C, double M, double U) {
+        this.M = M;
+        this.C = C;
+        this.U = U;
+    }
+}
---
+++ src/main/java/io/github/jimregan/jcanuint/Model.java
@@ -0,0 +1,46 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018 Jim O'Regan
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package io.github.jimregan.jcanuint;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Model {
+    private Map<String, Element> words;
+    private Map<String, Element> regexes;
+    public Model() {
+        this.words = new HashMap<String, Element>();
+        this.regexes = new HashMap<String, Element>();
+    }
+    static String getSimpleString(String s) {
+        String work = s;
+        if(s.charAt(0) == '^' && s.endsWith("[ ]")) {
+            return s.substring(1, s.length() - 3);
+        }
+        return s;
+    }
+    private static boolean isRegexLike(String s) {
+        return s.contains("(") || s.contains("?") || s.contains("[");
+    }
+}
